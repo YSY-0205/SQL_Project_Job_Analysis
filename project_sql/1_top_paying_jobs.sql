26dba64150651e2179406d40f31ab34b0ec1a195
Ethan <z5436580@ad.unsw.edu.au>
2026-04-20 16:22:37 +1000

    updating the first query to software engineer

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -13,6 +13,6 @@ SELECT
 
 FROM job_postings_fact 
 LEFT JOIN company_dim on job_postings_fact.company_id = company_dim.company_id
-WHERE job_title_short = 'Data Analyst' AND job_location = 'Anywhere' AND salary_year_avg IS NOT NULL
+WHERE job_title_short = 'Software Engineer' AND job_location = 'Anywhere' AND salary_year_avg IS NOT NULL
 ORDER BY salary_year_avg DESC
 LIMIT 10
