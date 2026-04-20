5290518c498e8284b96f52a7d1364da293de5a11
Ethan <z5436580@ad.unsw.edu.au>
2026-04-20 16:59:48 +1000

    created a query to find the most optimal skills to learn as a software engineer

---
+++ project_sql/5_optimal_jobs.sql
@@ -0,0 +1,21 @@
+/*
+Finding the most optimal skills to learn (aka it's in high demand and a high paying skill)
+*/
+
+SELECT 
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Software Engineer' AND
+    salary_year_avg IS NOT NULL
+GROUP BY skills_dim.skill_id
+HAVING COUNT(skills_job_dim.job_id) > 10
+ORDER BY
+    avg_salary DESC, 
+    demand_count DESC
+LIMIT 25
